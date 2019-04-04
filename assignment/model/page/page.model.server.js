03f6b3104f47ab0e68bd767872abf232ac396b5b
austin <austinvanderwel16@gmail.com>
2019-04-04 04:58:12 -0700

    implemented page.model and page.schema, but still having problems finding mongoose functions like findandupdate findandremove ect.

---
+++ assignment/model/page/page.model.server.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var pageSchema = require('./page.schema.server');
+
+var pageModel = mongoose.model('Page', pageSchema);
+
+pageModel.createPage = createPage;
+pageModel.findAllPagesForWebsite = findAllPagesForWebsite;
+pageModel.findPageById = findPageById;
+pageModel.updatePage = updatePage;
+pageModel.deletePage = deletePage;
+
+function createPage(page) {
+    return pageModel.create(page);
+}
+
+function findAllPagesForWebsite(websiteId) {
+    return pageModel.find({websiteId: websiteId});
+}
+
+// TODO: findById????
+function findPageById(pageId) {
+    return pageModel.findById(pageId);
+}
+
+//TODO: findByIdAndUpdate????
+function updatePage(pageId, page) {
+    return pageModel.findByIdAndUpdate(pageIde, page);
+}
+
+function deletePage(pageId) {
+    return pageModel.findByIdAndRemove(pageId);
+}
